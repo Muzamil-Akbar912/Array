bee0e748814b1dc6856c9dc621894caeb8a41114
Muzamil-Akbar912 <darmuzamil006@gmail.com>
2026-09-05 00:18:39 +0530

    Added new question

---
+++ MissingNumberInArray.java
@@ -0,0 +1,15 @@
+public class MissingNumberInArray {
+    public static int missing(int arr[]) {
+        int n = arr.length + 1;
+        int sum = n*(n+1)/2;
+        int arraySum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arraySum += arr[i];
+        }
+        return sum - arraySum;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 6, 7};
+        System.out.println("The missing number in an array is " + missing(arr));
+    }
+}
